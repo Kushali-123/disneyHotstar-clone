657dea4497e76e43f8a54c0b35b2e8ed1b622eb2
Kushali Thaman <thamankushali@gmail.com>
2023-04-16 17:37:14 +0530

    Change case for linux

--- src/components/Viewers.jsx
+++ src/components/Viewers.jsx
@@ -5,31 +5,31 @@ function Viewers() {
   return (
     <Container>
        <Wrap>
-         <img src ="/images/Viewers-disney.png" />
+         <img src ="/images/viewers-disney.png" />
          <video muted = {true} autoPlay = {true} loop = {true} playsInline = {true}>
           <source src = "/videos/1564674844-disney.mp4"  type= 'video/mp4' />
          </video>
        </Wrap>
        <Wrap>
-         <img src ="/images/Viewers-pixar.png" />
+         <img src ="/images/viewers-pixar.png" />
          <video muted = {true} autoPlay = {true} loop = {true} playsInline = {true}>
           <source src = "/videos/1564676714-pixar.mp4"  type= 'video/mp4' />
          </video>
        </Wrap>
        <Wrap>
-         <img src ="/images/Viewers-marvel.png" />
+         <img src ="/images/viewers-marvel.png" />
          <video muted = {true} autoPlay = {true} loop = {true} playsInline = {true}>
           <source src = "/videos/1564676115-marvel.mp4"  type= 'video/mp4' />
          </video>
        </Wrap>
        <Wrap>
-         <img src ="/images/Viewers-starwars.png" />
+         <img src ="/images/viewers-starwars.png" />
          <video muted = {true} autoPlay = {true} loop = {true} playsInline = {true}>
           <source src = "/videos/1608229455-star-wars.mp4"  type= 'video/mp4' />
          </video>
        </Wrap>
        <Wrap>
-         <img src ="/images/Viewers-national.png" />
+         <img src ="/images/viewers-national.png" />
          <video muted = {true} autoPlay = {true} loop = {true} playsInline = {true}>
           <source src = "/videos/1564676296-national-geographic.mp4"  type= 'video/mp4' />
          </video>
