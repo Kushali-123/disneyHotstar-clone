7d3e628266a0f4bf5d5f04be6f4ff4200355e3c8
Kushali Thaman <thamankushali@gmail.com>
2023-04-16 20:13:29 +0530

    firebase changed

--- src/firebase.js
+++ src/firebase.js
@@ -6,14 +6,15 @@ import "firebase/compat/storage";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDzAOWZIT3vS3AXGMXeDb8tUOKLdxx11l4",
-  authDomain: "disneyplus-clone-a175e.firebaseapp.com",
-  projectId: "disneyplus-clone-a175e",
-  storageBucket: "disneyplus-clone-a175e.appspot.com",
-  messagingSenderId: "319436798168",
-  appId: "1:319436798168:web:897a0ec809f783c719bfad",
-  measurementId: "G-685EHPF4BG"
+  apiKey: "AIzaSyDwSa94pGdsWfcZQSJ8lk0P5-BGqSxXWRY",
+  authDomain: "disney-clone-clone.firebaseapp.com",
+  projectId: "disney-clone-clone",
+  storageBucket: "disney-clone-clone.appspot.com",
+  messagingSenderId: "848363696642",
+  appId: "1:848363696642:web:c376783cb90f7232bb6d1b",
+  measurementId: "G-C2TS7XBJBP"
 };
+
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
